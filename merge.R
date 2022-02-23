4db7693a0b5cb43cc63509bd69eb0e842a5c623e
hkwilliamchiu <77698891+hkwilliamchiu@users.noreply.github.com>
2022-02-23 13:49:46 -0600

    Add files via upload

--- merge.R
+++ merge.R
@@ -1,32 +1,26 @@
 # Author: William Chiu
-# Date: Feb 18, 2022
-# This is a very preliminary draft with limited capability/flexibility; more updates to come
-# to-do
-# decide on what company parameters to report (and how to call them)
-# Name, ISIN, Industry, Country
-# Bloomberg: Name, ISIN, Sub_Industry, cntry_of_incorporation
-# Refinitiv: Company Name, ISIN, ..., Country of Headquarters
-# Sustainalytics: EntityName, ISIN, Subindustry, Country
-
-# create global reference file?
-
-# possible primary merger: ISIN
-
-# support KLD social ratings
-
-# before merging, need to ascertain: what files are fed, what company parameters to use
+# Date: Feb 23, 2022
+# This is a preliminary draft; more updates to come
 
 # set working directory to source file location
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 rm(list = ls())
+#source("https://raw.githubusercontent.com/hkwilliamchiu/Merge-ESG-Data/main/functions.R")
 source("functions.R")
 
 custom_list <- list(
-  bloomberg_data = 1,
-  refinitiv_data = 1,
-  sustainalytics_data = 1,
-  # Choose one of the following supported identifier: ISIN
+  bloomberg = 1,
+  refinitiv = 1,
+  sustainalytics = 1,
+  # ISIN recommended
   identifier = "ISIN"
 )
 
-merge_data(custom_list)
+coalesce_names <- data.frame(
+  row.names = c("bloomberg","refinitiv","sustainalytics"),
+  company_name = c("Name","Company Name","EntityName"),
+  country = c("cntry_of_incorporation","Country of Headquarters","Country"),
+  industry = c("Sub_Industry",NA,"Subindustry")
+)
+
+merge_data(custom_list, coalesce_names)
